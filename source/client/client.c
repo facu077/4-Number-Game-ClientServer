6a0be99034e1ef8f884db357d35d34abeb3a6145
Facundo <facu.m077@gmail.com>
2019-10-14 19:20:15 -0300

    Improved Client - Server communication

--- source/client/client.c
+++ source/client/client.c
@@ -1,16 +1,17 @@
-#include<stdio.h> //printf
-#include<string.h> //strlen
-#include<sys/socket.h> //socket
-#include<arpa/inet.h> //inet_addr
-#include <unistd.h> // for close
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
  
 int main(int argc , char *argv[])
 {
     int sock, read_size;
+    int keep_playing = 1;
     struct sockaddr_in server;
     char message[1000] , server_reply[2000];
      
-    //Create socket
+    // Create socket
     sock = socket(AF_INET , SOCK_STREAM , 0);
     if (sock == -1)
     {
@@ -22,7 +23,7 @@ int main(int argc , char *argv[])
     server.sin_family = AF_INET;
     server.sin_port = htons( 8888 );
  
-    //Connect to remote server
+    // Connect to server
     if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
     {
         perror("connect failed. Error");
@@ -39,22 +40,30 @@ int main(int argc , char *argv[])
     scanf("%s", message);
     write(sock, message, strlen(message));
 
-    //keep communicating with server
-    while(strcmp(server_reply, "-1") != 0)
+    // keep communicating with server
+    while(keep_playing == 1)
     {
         // read from the server
         read_size = read(sock, server_reply, sizeof server_reply);
         if (read_size < 0)
         {
-            puts("recv failed");
+            puts("read failed");
         }
-        puts("/n");
-        write(STDOUT_FILENO, server_reply, read_size);
-        // write to the server
-        scanf("%s", message);
-        write(sock, message, strlen(message));
+        if (strcmp(server_reply, "-1") == 0)
+        {
+            puts("Thanks for playing :)\n");
+            keep_playing = 0;
+        }
+        else
+        {
+            // write to the user
+            write(STDOUT_FILENO, server_reply, read_size);
+            // write user answer to the server
+            scanf("%s", message);
+            write(sock, message, strlen(message));
+        }
     }
-    
     close(sock);
+    puts("Server socket closed\n");
     return 0;
 }
