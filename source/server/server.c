0a5b71f7129ca5104387c31f399bcfdac02ccd1c
Facundo <facu.m077@gmail.com>
2019-10-19 19:40:50 -0300

    Refactoring and multiThreads implementation

--- source/server/server.c
+++ source/server/server.c
@@ -4,32 +4,14 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <pthread.h>
 #include "service.h"
-
-// TODO avoid this global variable
-int pos = 0;
-
-void * calculate_number(void * args)
-{
-    int new_number, last_number;
-	printf("I'm the thread!\n");
-    Guess * guesses = args;
-    printf("I have: good: %d, regular: %d, new number: %s\n",guesses[pos].good, guesses[pos].regular, guesses[pos].number);
-    last_number = atoi(guesses[pos].number);
-    new_number = last_number + 1;
-
-    // TODO check if it is really necessary that guess.number be an string
-    sprintf(guesses[pos+1].number, "%d", new_number);
-	pthread_exit(NULL);
-}
-
 
 int main(int argc , char *argv[])
 {
-    int socket_desc, client_sock, c;
+    int socket_desc, client_sock, c, pos;
     struct sockaddr_in client;
     Guess * guesses;
+    Thread_data data;
     char client_message[2000];
     char server_message[2000];
     int keep_playing = 1;
@@ -54,11 +36,11 @@ int main(int argc , char *argv[])
             // TODO Look in the log.txt file for the user data
             // TODO Generate random number
             strcpy(guesses[0].number, "1234");
-            // TODO Improve this strcpy, strcat,strcat...
+            // TODO Improve this (and all) strcpy, strcat,strcat...
             strcpy(server_message, "Welcome ");
             strcat(server_message, client_message);
             strcat(server_message, ", you have played 8 times, with an average of 4 attempts per correct answer\n");
-            strcpy(server_message, "Is your number ");
+            strcat(server_message, "Is your number ");
             strcat(server_message, guesses[0].number);
             strcat(server_message, "?\n");
             // Set welcome message to client with a random number
@@ -82,14 +64,16 @@ int main(int argc , char *argv[])
                     // Ask for good numbers
                     guesses[pos].good = atoi(writeAndRead(client_sock, "Good numbers?\n"));
                     // TODO Generate new number using threads
-                    pthread_t tid;
-                    if(pthread_create(&tid, NULL, calculate_number, guesses) != 0)
+                    data.guesses = guesses;
+                    data.pos = pos;
+                    //  Run the threads that will find the new number
+                    if ((run_threads(data, 2) != 0))
                     {
-                        printf("Error creating thread\n");
+                        printf("Error creating threads\n");
                         return -1;
                     }
-                    pthread_join(tid,NULL);
                     pos++;
+                    // Todo improve this message generation
                     strcpy(server_message, "Is your number ");
                     strcat(server_message, guesses[pos].number);
                     strcat(server_message, "?\n");
