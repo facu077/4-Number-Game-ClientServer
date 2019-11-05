3b36e7d67a306d535cc593ecda53052979707f7c
Facundo <facu.m077@gmail.com>
2019-11-04 21:39:46 -0300

    Added Log process

--- source/server/server.c
+++ source/server/server.c
@@ -5,23 +5,56 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <netdb.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 #include "service.h"
 
 int main(int argc , char *argv[])
 {
-    int socket_desc, client_sock, client_len, pos;
+    int socket_desc, client_sock, client_len, pos, pipe_fd[2];
     struct sockaddr_in client;
-    Guess * guesses;
-    Thread_data data;
-    char client_message[2000];
-    char server_message[2000];
-    int keep_playing = 1;
     char ip[INET6_ADDRSTRLEN];
-    pid_t pid;
+    char * port;
+    char * inter_message;
+    pid_t pid, logger;
 
     socket_desc = start_server();
     client_len = sizeof(struct sockaddr_in);
-    guesses = malloc(10 * sizeof *guesses);
+    inter_message = calloc(200, sizeof(char));
+    port = calloc(6, sizeof(char));
+    // memset(inter_message, 0, sizeof inter_message);
+
+    if (pipe(pipe_fd) < 0)
+    {
+		perror("pipe()");
+		return -1;
+	}
+
+    logger = fork();
+    if (logger == 0)
+    {
+        int fd, read_size, keep_listening = 1;
+        puts("Logger process initialized");
+        fd = open ("log.txt",O_RDWR|O_APPEND|O_CREAT, S_IRUSR|S_IWUSR);
+        while (keep_listening == 1)
+        {
+            read_size = read(pipe_fd[0], inter_message, sizeof inter_message);
+            if (strcmp(inter_message, "-1") == 0)
+            {
+                keep_listening = 0;
+            }
+            else
+            {
+                write(fd, inter_message, read_size);
+            }
+        }
+        free(inter_message);
+        puts("Logger process finished");
+        return 0;
+    }
 
     //Accept connection from an incoming client
     while ((client_sock = accept(socket_desc,(struct sockaddr *)&client,(socklen_t*)&client_len))>0)
@@ -33,21 +66,39 @@ int main(int argc , char *argv[])
             perror("Cannot getaddrinfo");
             return -1;
         }
-        printf("Client IP: %s and PORT: %d\n", ip, ntohs(client.sin_port));
-
+        // port = ntohs(client.sin_port);
+        sprintf(port, "%d", ntohs(client.sin_port));
+        printf("Client IP: %s and PORT: %s\n", ip, port);
 
         pid = fork();
         if (pid == 0)
         {
+            Guess * guesses;
+            Thread_data data;
+            char client_message[2000], server_message[2000];
+            int keep_playing = 1;
+
+            guesses = malloc(10 * sizeof *guesses);
+
             // Ask client name
             strncpy(client_message, writeAndRead(client_sock, "Please enter your name: "), sizeof(client_message));
             // TODO Look in the log.txt file for the user data
+            // Write user data to log.txt
+            strncpy(inter_message, "Player: ", 9);
+            strncat(inter_message, client_message, sizeof(client_message));
+            strncat(inter_message, "; IP: ", 7);
+            strncat(inter_message, ip, sizeof ip);
+            strncat(inter_message, "; PORT: ", 9);
+            strncat(inter_message, port, strlen(port));
+            strncat(inter_message, ";\n", 3);
+            write(pipe_fd[1], inter_message, strlen(inter_message));
             // TODO Generate random number
             strncpy(guesses[0].number, "1234", 5);
             // TODO Improve this (and all) strcpy, strcat,strcat...
             strncpy(server_message, "Welcome ", 9);
             strncat(server_message, client_message, sizeof(client_message));
-            strncat(server_message, ", you have played 8 times, with an average of 4 attempts per correct answer\n", 77);
+            // strncat(server_message, ", you have played 8 times, with an average of 4 attempts per correct answer\n", 77);
+            strncat(server_message, "\n", 2);
             strncat(server_message, "Is your number ", 16);
             strncat(server_message, guesses[0].number, 4);
             strncat(server_message, "?\n", 3);
@@ -92,12 +143,13 @@ int main(int argc , char *argv[])
 
             close(client_sock);
             puts("Client socket closed");
-
+            free(inter_message);
             free(guesses);
             return 0;
         }
     }
     close(socket_desc);
+    kill(logger, SIGTERM);
     puts("Server socket closed");
     return 0;
 }
